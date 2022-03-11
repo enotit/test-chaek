33fe44ba82a0702733b2fd4c0f826319d40c8144
enotit <43219870+enotit@users.noreply.github.com>
2022-03-11 22:34:57 +0300

    start shopping cart

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="">
+    <ShoppingCart/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ShoppingCart from "@/components/ShoppingCart"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ShoppingCart
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;0,600;1,300&display=swap');
+  html, body{
+    background-color: rgb(243, 244, 246);
+    outline:none;
+    font-family: 'Montserrat', sans-serif;
+  }
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0; /* <-- Apparently some margin are still there even though it's hidden */
 }
 </style>
---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="ShoppingCart" class="bg-gray-200 h-128 w-64 rounded-2xl border border-gray-500">
+        
+        <div class="m-5 flex flex-col">
+            <span class="font-semibold"> Название </span>
+            <div class="">
+                <span class="font-thin opacity-70">{{300}}р. </span>
+                <button class="text-2xl"> - </button>
+                <input type="number" name="" value="0" id="" class="w-8 text-gray-500/50 rounded-1xl center ml-3 mr-3 text-center">
+                <button class="text-2xl"> + </button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+
+</script>
